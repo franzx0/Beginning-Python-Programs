3378eee89db41d99cb2ed6d5802621fdeaafb1a5
Alexandre Franz Pries <57185379+franzx0@users.noreply.github.com>
2025-02-19 23:20:08 -0300

    Add files via upload

---
+++ 7.1.py
@@ -0,0 +1,5 @@
+# Solicita ao usuário que informe o tipo de carro que deseja alugar
+carro_desejado = input("Que tipo de carro você gostaria de alugar? ")
+
+# Exibe uma mensagem personalizada com o carro informado
+print(f"Deixe-me ver se consigo um {carro_desejado.title()} para você.")
